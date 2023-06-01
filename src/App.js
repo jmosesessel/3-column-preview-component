ca87f7fa178654fee060caf5a52e34e8e574bf65
Joseph Essel Moses <jmosesessel21@gmail.com>
2023-06-01 22:29:47 +0000

    fixed h1 error

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import MainPage from "./view/MainPage";
 function App() {
 	return (
 		<div className="App">
+			<h1 className="sr-only">3-column preview card component</h1>
 			<MainPage></MainPage>
 		</div>
 	);
